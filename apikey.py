61d4d1b6876d8dac160f40f7f91c743d4ac4acd2
SUNNAM_SRIRAM <59051820+sunnamsriram1@users.noreply.github.com>
2025-08-06 16:41:40 +0530

    Delete apikey.py

--- apikey.py
+++
@@ -1,6 +0,0 @@
-
-# -------------------------
-# NUMVERIFY SETTINGS & FUNCTIONS (official API version)
-# -------------------------
-# Numverify API Key (Replace with your valid key)
-NUMVERIFY_API_KEY = "e77d0f81018bb99e402b96bc9ead7d6b"
